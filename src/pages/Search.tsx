9be6185e98f97f9a61cef7872183523cee697c3f
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-13 23:31:17 +0200

    testing query params

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,27 @@
+import React, { useEffect, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
 import { MainContainer, MainContainerCard } from '../components';
 
 export const Search: React.FC = () => {
+  const useQuery = () => {
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  };
+
+  const queryCategories = useQuery().get('cats');
+  const queryImgs = useQuery().get('imgs');
+  console.log('queryCategories', queryCategories);
+  console.log('queryImgs', queryImgs);
+
   return (
     <>
       <MainContainer>
         <MainContainerCard>
-          <p>Test</p>
+          <>
+            <h1 style={{ color: 'white' }}>Test</h1>
+            <h3 style={{ color: 'cyan' }}>Categories: {queryCategories}</h3>
+            <h3 style={{ color: 'cyan' }}>Images: {queryImgs}</h3>
+          </>
         </MainContainerCard>
       </MainContainer>
     </>
